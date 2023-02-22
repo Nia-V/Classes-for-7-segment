1a58a2f0ae33d87fee8f0a453db667c190c99a17
solonovamax <solonovamax@12oclockpoint.com>
2023-02-22 17:59:32 -0500

    Code cleanup
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- Thank you, Friend.ino
+++ Thank you, Friend.ino
@@ -1,67 +1,50 @@
-
-
-class Seg {
+class SevenSegmentDisplay {
 private:
-  int* pins; // ""int*"" means a pointer to a variable whose datatype is integer" I dont really understand this at all. But it works??
-  
-  Seg(){};
+    int *pins; // ""int*"" means a pointer to a variable whose datatype is integer" I dont really understand this at all. But it works??
+
+    SevenSegmentDisplay() {};
 public:
-  Seg (int pins[7]) {
-    this->pins = pins;
-  }
-
-  void init(){
-    for (int i = 0; i<8; i++){
-      pinMode(pins[i],OUTPUT);
+    SevenSegmentDisplay(int pins[7]) {
+        this->pins = pins;
     }
-  }
 
-
-  void display(int num) {
-    int numbers[10][7] = {
-      {1,1,1,1,1,1,0},
-      {0,1,1,0,0,0,0},
-      {1,1,0,1,1,0,1},
-      {1,1,1,1,0,0,1},
-      {0,1,1,0,0,1,1},
-      {1,0,1,1,0,1,1},
-      {1,0,1,1,1,1,1},
-      {1,1,1,0,0,0,0},
-      {1,1,1,1,1,1,1},
-      {1,1,1,1,0,1,1}
-    };
-    for (int i = 0; i<8; i++){
-      digitalWrite(pins[i], numbers[num][i]);
+    void init() {
+        for (int i = 0; i < 8; i++) {
+            pinMode(pins[i], OUTPUT);
+        }
     }
-  }
+
+
+    void display(int num) {
+        int numbers[10][7] = {
+                {1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 0, 0, 0, 0},
+                {1, 1, 0, 1, 1, 0, 1},
+                {1, 1, 1, 1, 0, 0, 1},
+                {0, 1, 1, 0, 0, 1, 1},
+                {1, 0, 1, 1, 0, 1, 1},
+                {1, 0, 1, 1, 1, 1, 1},
+                {1, 1, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 0, 1, 1}
+        };
+
+        for (int i = 0; i < 7; i++) {
+            digitalWrite(pins[i], numbers[num][i]);
+        }
+    }
 };
 
-int my7pins[7] {2,3,4,5,6,7,8};
-Seg my7(my7pins);
+int my7pins[7]{2, 3, 4, 5, 6, 7, 8};
+SevenSegmentDisplay my7(my7pins);
 
-  void setup() {
+void setup() {
     my7.init();
-  }
+}
 
-  void loop() {
-    my7.display(0);
-    delay(1000);
-    my7.display(1);
-    delay(1000);
-    my7.display(2);
-    delay(1000);
-    my7.display(3);
-    delay(1000);
-    my7.display(4);
-    delay(1000);
-    my7.display(5);
-    delay(1000);  
-    my7.display(6);
-    delay(1000);
-    my7.display(7);
-    delay(1000);
-    my7.display(8);
-    delay(1000);
-    my7.display(9);
-    delay(1000);
-  }
+void loop() {
+    for (int i = 0; i < 10; ++i) {
+        my7.display(i);
+        delay(1000);
+    }
+}
